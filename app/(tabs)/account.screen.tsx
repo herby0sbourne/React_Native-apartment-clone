30b8b3cbe2624cd4da29c0a40a5541c2e9a0c287
Herby <herbyosbourne31@gmail.com>
2024-06-18 19:54:07 -0500

    create screen

---
+++ app/(tabs)/account.screen.tsx
@@ -0,0 +1,11 @@
+import SafeArea from "@/components/SafeArea";
+import { StyleSheet, Text, View } from "react-native";
+const Page = () => {
+  return (
+    <SafeArea>
+      <Text>ACCOUNT</Text>
+    </SafeArea>
+  );
+};
+export default Page;
+const styles = StyleSheet.create({});
