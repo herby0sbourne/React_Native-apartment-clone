5cf722e7ed250a07bef5e5f5d00d3a121c170ad8
Herby <herbyosbourne31@gmail.com>
2024-06-18 19:52:55 -0500

    added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  endOfLine: "auto",
+  printWidth: 90,
+};
