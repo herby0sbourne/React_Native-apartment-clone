83b2c9296333899fd38bf4bd7f9e3635d392bcba
Herby <herbyosbourne31@gmail.com>
2024-06-18 19:53:54 -0500

    cusstom safe are compoent

---
+++ components/SafeArea.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, ViewStyle } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface SafeAreaProps {
+  children: React.ReactNode;
+  style?: ViewStyle;
+}
+
+const SafeArea = ({ children, style }: SafeAreaProps) => {
+  return <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default SafeArea;
