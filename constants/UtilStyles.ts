f1ac469fcd7386d7fb1b822b57793d3948c65de1
Herby <herbyosbourne31@gmail.com>
2024-06-18 19:51:38 -0500

    custom colors and utils

---
+++ constants/UtilStyles.ts
@@ -0,0 +1,11 @@
+import { ViewStyle } from "react-native";
+
+const utils: { row: ViewStyle } = {
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
+
+export default utils;
