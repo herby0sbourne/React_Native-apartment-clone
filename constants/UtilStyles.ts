e305d6ea5e72c1617046efa2c2d2c65f0d815e6c
Herby <herbyosbourne31@gmail.com>
2024-06-20 14:55:00 -0500

    added flex util

--- constants/UtilStyles.ts
+++ constants/UtilStyles.ts
@@ -1,9 +1,13 @@
 import { ViewStyle } from "react-native";
 
-const utils: { row: ViewStyle } = {
+const utils: { row: ViewStyle; flex: ViewStyle } = {
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+  },
+  flex: {
+    flexDirection: "row",
     alignItems: "center",
   },
 };
