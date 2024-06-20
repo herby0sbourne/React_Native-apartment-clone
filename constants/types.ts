76de01b650e1605f0a3b0f6ad82e3117f2135f1d
Herby <herbyosbourne31@gmail.com>
2024-06-20 12:36:18 -0500

    types file

---
+++ constants/types.ts
@@ -0,0 +1,14 @@
+export interface Property {
+  id: string;
+  images: string[];
+  rentLow: number;
+  rentHigh: number;
+  bedRoomLow: number;
+  bedRoomHigh: number;
+  name: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  tags: string[];
+}
