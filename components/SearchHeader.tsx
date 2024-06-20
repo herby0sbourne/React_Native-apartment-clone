248ac8df52030ae9097266c122d8d3ac811c281c
Herby <herbyosbourne31@gmail.com>
2024-06-20 14:54:50 -0500

    complete searchHeader

--- components/SearchHeader.tsx
+++ components/SearchHeader.tsx
@@ -1,14 +1,51 @@
-import { StyleSheet, Text } from "react-native";
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useLayoutEffect } from "react";
 import Animated, {
+  SharedValue,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
 
 import { HEADER_HEIGHT, TRANSITION_THRESHOLD } from "@/constants/variable";
-import { useLayoutEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Colors from "@/constants/Colors";
+import { gray } from "colorette";
+import utilStyles from "@/constants/UtilStyles";
+import UtilStyles from "@/constants/UtilStyles";
 
-const SearchHeader = ({ scrollOffset }) => {
+interface SearchHeaderProps {
+  scrollOffset: SharedValue<number>;
+}
+
+const filterBtn = [
+  {
+    label: "Price",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Beds & Bath",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Move-in Date",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Pets",
+    onPress: () => console.log("price press"),
+  },
+];
+
+const SearchHeader = ({ scrollOffset }: SearchHeaderProps) => {
   const previousScrollOffset = useSharedValue(0);
   const accumulatedScrollUp = useSharedValue(0);
   const headerYPosition = useSharedValue(0);
@@ -48,7 +85,60 @@ const SearchHeader = ({ scrollOffset }) => {
 
   return (
     <Animated.View style={[styles.container, headerAnimatedStyle]}>
-      <Text>apple</Text>
+      <View style={{ marginHorizontal: 10, paddingTop: 10, gap: 15 }}>
+        <TouchableOpacity onPress={() => console.log("go to input screen pressed")}>
+          <View style={styles.searchBar}>
+            <Ionicons name={"search-outline"} size={28} color={Colors.primary} />
+            <Text style={styles.searchText}>Find Location</Text>
+          </View>
+        </TouchableOpacity>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ gap: 10 }}
+        >
+          <View style={styles.btnOutline}>
+            <Ionicons name={"filter"} size={20} color={Colors.primary} />
+          </View>
+          {filterBtn.map(item => (
+            <TouchableOpacity
+              key={item.label}
+              style={[styles.btnOutline, { paddingHorizontal: 10, borderRadius: 15 }]}
+              onPress={item.onPress}
+            >
+              <Text style={styles.filterText}>{item.label}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+        <View>
+          <View style={[styles.divider]} />
+          <View style={[utilStyles.row, { marginTop: 8 }]}>
+            <View style={[UtilStyles.flex, { gap: 5 }]}>
+              <FontAwesome name="map-marker" size={18} color={Colors.primary} />
+              <Text style={{ color: "gray" }}>12 Available</Text>
+              <TouchableOpacity onPress={() => console.log("save pressed")}>
+                <Text style={styles.btnTitle}>Save</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={[utilStyles.flex, { gap: 20 }]}>
+              <TouchableOpacity
+                onPress={() => console.log("save pressed")}
+                style={[utilStyles.flex, { gap: 5 }]}
+              >
+                <Ionicons name={"map-outline"} size={20} color={Colors.info} />
+                <Text style={styles.btnTitle}>Sort</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => console.log("save pressed")}
+                style={[utilStyles.flex, { gap: 5 }]}
+              >
+                <Ionicons name={"map-outline"} size={20} color={Colors.info} />
+                <Text style={styles.btnTitle}>Map</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
     </Animated.View>
   );
 };
@@ -59,8 +149,37 @@ const styles = StyleSheet.create({
     height: HEADER_HEIGHT - 24,
     zIndex: 10,
     // top: 24,
-    backgroundColor: "pink",
+    // backgroundColor: "pink",
+    backgroundColor: "white",
   },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    borderWidth: 1,
+    borderColor: "lightgray",
+    borderRadius: 20,
+    padding: 7,
+  },
+  searchText: {
+    fontWeight: "400",
+    fontSize: 16,
+  },
+  btnOutline: {
+    borderColor: "lightgray",
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 5,
+  },
+  filterText: {
+    fontWeight: "600",
+    color: Colors.primary,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: "#E0E0E0", // Light grey color
+    width: "100%",
+  },
+  btnTitle: { color: Colors.info, fontWeight: "600" },
 });
 export default SearchHeader;
-// { transform: [{ translateY: -50 }] }
