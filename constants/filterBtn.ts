12ad0096bb80e649d8c2f79a9b69d4eafbb9c058
Herby <herbyosbourne31@gmail.com>
2024-06-21 08:40:38 -0500

    added filter button file

---
+++ constants/filterBtn.ts
@@ -0,0 +1,18 @@
+export const filterBtn = [
+  {
+    label: "Price",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Beds & Bath",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Move-in Date",
+    onPress: () => console.log("price press"),
+  },
+  {
+    label: "Pets",
+    onPress: () => console.log("price press"),
+  },
+];
