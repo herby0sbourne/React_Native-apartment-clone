4a41f6c239846cb6c4452a03e756dfc8f3e82dc0
Herby <herbyosbourne31@gmail.com>
2024-06-21 08:50:32 -0500

    update home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import SafeArea from "@/components/SafeArea";
-import { StatusBar, StyleSheet } from "react-native";
+import { StatusBar, StyleSheet, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
 
 import PropertyCard from "@/components/PropertyCard";
 import SearchHeader from "@/components/SearchHeader";
@@ -12,21 +13,23 @@ const Page = () => {
   const scrollOffset = useScrollViewOffset(flatListRef);
 
   return (
-    <SafeArea style={{ paddingHorizontal: 10 }}>
-      <StatusBar translucent={false} />
-      <SearchHeader scrollOffset={scrollOffset} />
+    <>
+      <StatusBar translucent={false} barStyle={"dark-content"} />
+      <SafeArea style={{ paddingHorizontal: 10 }}>
+        <SearchHeader scrollOffset={scrollOffset} />
 
-      <Animated.FlatList
-        ref={flatListRef}
-        data={properties}
-        renderItem={({ item }) => <PropertyCard property={item} />}
-        keyExtractor={item => item.id}
-        contentContainerStyle={[{ gap: 10 }]}
-        showsVerticalScrollIndicator={false}
-        scrollEventThrottle={16}
-        bounces={false}
-      />
-    </SafeArea>
+        <Animated.FlatList
+          ref={flatListRef}
+          data={properties}
+          renderItem={({ item }) => <PropertyCard property={item} />}
+          keyExtractor={item => item.id}
+          contentContainerStyle={[{ gap: 10 }]}
+          showsVerticalScrollIndicator={false}
+          scrollEventThrottle={16}
+          bounces={false}
+        />
+      </SafeArea>
+    </>
   );
 };
 
