68665c91b6dfcf4453049785579d17c4dc152bf4
Herby <herbyosbourne31@gmail.com>
2024-06-25 00:34:58 -0500

    update data file and property types

--- constants/types.ts
+++ constants/types.ts
@@ -1,14 +1,68 @@
 export interface Property {
-  id: string;
+  id: number;
+  unitType: string;
   images: string[];
+  about: string;
   rentLow: number;
   rentHigh: number;
-  bedRoomLow: number;
-  bedRoomHigh: number;
+  bedroomLow: number;
+  bedroomHigh: number;
   name: string;
   street: string;
   city: string;
   state: string;
-  zip: string;
+  zip: number;
   tags: string[];
+  lat: number;
+  lng: number;
+  pets: Pet[];
+  apartments: Apartment[];
+  features: string[];
+  phoneNumber: string;
+  website: string;
+  scores: Score[];
+  stars: number;
+  reviews?: Review[];
+}
+
+interface Review {
+  body: string;
+  CreatedAt: string;
+  id: number;
+  propertyID: number;
+  stars: number;
+  title: string;
+  userID: number;
+  DeletedAt: null;
+  UpdatedAt: null;
+}
+
+interface Score {
+  type: string;
+  description: string;
+  score: number;
+}
+
+interface Apartment {
+  CreatedAt: string;
+  DeletedAt: null;
+  id: number;
+  UpdatedAt: string;
+  bathrooms: number;
+  bedrooms: number;
+  images: string[];
+  propertyID: number;
+  rent: number;
+  sqFt: number;
+  unit: string;
+}
+
+interface Pet {
+  type: string;
+  allowed: boolean;
+  details: string;
+  limit: number;
+  fee: number;
+  deposit: number;
+  rent: number;
 }
