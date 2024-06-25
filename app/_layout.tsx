53ac644576a2e75cb631baf187610667017cde68
Herby <herbyosbourne31@gmail.com>
2024-06-25 12:13:34 -0500

    added Map screen to stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -46,6 +46,7 @@ function RootLayoutNav() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="map.screen" />
       {/* <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
     </Stack>
   );
