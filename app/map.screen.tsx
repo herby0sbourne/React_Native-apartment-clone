c5a6f54f38b3d56fb1e0aed7e6df721424a7bec4
Herby <herbyosbourne31@gmail.com>
2024-06-28 23:44:43 -0500

    added property card to map

--- app/map.screen.tsx
+++ app/map.screen.tsx
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { Property } from "@/constants/types";
 import MapMarker from "@/components/MapMarker";
 import Colors from "@/constants/Colors";
@@ -8,6 +8,7 @@ import { Stack } from "expo-router";
 import { useRoute } from "@react-navigation/core";
 
 import SearchHeader from "@/components/SearchHeader";
+import PropertyCard from "@/components/PropertyCard";
 
 interface MapProps {
   properties: Property[];
@@ -16,8 +17,10 @@ interface MapProps {
 const INITIAL_REGION = {
   latitude: 25.80913,
   longitude: -80.186363,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.1,
+  // latitudeDelta: 0.0922,
+  // longitudeDelta: 0.0421,
 };
 
 const MapScreen = ({}: MapProps) => {
@@ -37,7 +40,7 @@ const MapScreen = ({}: MapProps) => {
             longitude: lng,
           },
         },
-        { duration: 1 * 1000 },
+        { duration: 1000 },
       );
     }
     setActiveMarker(index);
@@ -63,6 +66,9 @@ const MapScreen = ({}: MapProps) => {
           );
         })}
       </MapView>
+      {activeMarker !== null && (
+        <PropertyCard property={properties[activeMarker]} extraStyle={styles.mapCard} />
+      )}
     </View>
   );
 };
@@ -70,11 +76,18 @@ const MapScreen = ({}: MapProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: "relative",
     // ...StyleSheet.absoluteFillObject,
   },
   mapStyle: {
     width: "100%",
     height: "100%",
+  },
+  mapCard: {
+    position: "absolute",
+    bottom: 10,
+    // height: 250,
+    marginHorizontal: 10,
   },
 });
 export default MapScreen;
