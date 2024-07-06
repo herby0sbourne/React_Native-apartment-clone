6abeb23656d34920b95dc2976d8d78319e7cb26e
Herby <herbyosbourne31@gmail.com>
2024-07-06 10:50:40 -0500

    API path variable

--- constants/variable.ts
+++ constants/variable.ts
@@ -1,4 +1,13 @@
 const HEADER_HEIGHT = 250;
 const TRANSITION_THRESHOLD = 100;
 
-export { HEADER_HEIGHT, TRANSITION_THRESHOLD };
+const SERVER_URL = "http://192.168.0.12:4000/api";
+const location = "/location";
+const locationEndpoint = SERVER_URL + location;
+
+const ENDPOINTS = {
+  searchEndpoint: `${locationEndpoint}/search`,
+  autocompleteEndpoint: `${locationEndpoint}/autocomplete`,
+};
+
+export { HEADER_HEIGHT, TRANSITION_THRESHOLD, ENDPOINTS };
