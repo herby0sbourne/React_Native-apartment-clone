1dae3c0312eb9541b8314aa918ae74bf2c151662
Herby <herbyosbourne31@gmail.com>
2024-07-13 11:18:22 -0500

    added switch for ip base on device in dev mode

--- constants/variable.ts
+++ constants/variable.ts
@@ -1,7 +1,11 @@
+import { Platform } from "react-native";
+
 const HEADER_HEIGHT = 250;
 const TRANSITION_THRESHOLD = 100;
 
-const SERVER_URL = "http://192.168.0.12:4000/api";
+const IPAddress = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+const SERVER_URL = `http://${IPAddress}:4000/api`;
+// const SERVER_URL = "http://192.168.100.7:4000/api";
 const location = "/location";
 const locationEndpoint = SERVER_URL + location;
 
