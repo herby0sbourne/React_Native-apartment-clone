0221249bef39e57244293e8a5ca7c934e735d59d
Herby <herbyosbourne31@gmail.com>
2024-08-01 16:25:25 -0500

    create component to show lottie animation if content is empty

---
+++ components/EmptyContent.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from "react-native";
+import LottieView, { AnimationObject } from "lottie-react-native";
+
+interface EmptyContentProps {
+  filePath: string | AnimationObject | { uri: string };
+  title: string;
+  subTitle: string;
+}
+
+const EmptyContent = ({ filePath, subTitle, title }: EmptyContentProps) => {
+  return (
+    <>
+      <LottieView autoPlay style={styles.lottie} source={filePath} />
+      <View style={styles.textWrapper}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subTitle}>{subTitle}</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  lottieWrapper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  lottie: {
+    width: 180,
+    height: 180,
+  },
+  textWrapper: {
+    marginVertical: 15,
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  subTitle: {
+    marginTop: 10,
+    fontSize: 15,
+    color: "gray",
+  },
+});
+
+export default EmptyContent;
