8843379e5dce4062de01674cc3b5e96b0ff1ad52
Herby <herbyosbourne31@gmail.com>
2024-08-27 15:57:39 -0500

    Update UtilStyles for improved layout and styling
    - Added new styles for better alignment and spacing.
    - Adjusted authBtn and authText styles for consistency.

--- constants/UtilStyles.ts
+++ constants/UtilStyles.ts
@@ -1,6 +1,6 @@
-import { ViewStyle } from "react-native";
+import { TextStyle, ViewStyle } from "react-native";
 
-export const UtilStyles: { row: ViewStyle; flex: ViewStyle } = {
+export const UtilStyles: Record<string, ViewStyle | TextStyle> = {
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -9,5 +9,23 @@ export const UtilStyles: { row: ViewStyle; flex: ViewStyle } = {
   flex: {
     flexDirection: "row",
     alignItems: "center",
+  },
+  authBtn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: 50,
+    borderRadius: 8,
+    backgroundColor: "white",
+    borderWidth: 0.5,
+  },
+  authText: {
+    color: "white",
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 15,
+    paddingLeft: 30,
   },
 };
