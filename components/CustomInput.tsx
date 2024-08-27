7439c4e42c5d70638835c32a28e81460ca1c059b
Herby <herbyosbourne31@gmail.com>
2024-08-27 15:56:31 -0500

    Add CustomInput component with password visibility toggle
    - Implemented CustomInput component for email and password input fields.
    - Added EyeIcon component to toggle password visibility.
    - Included props for handling text changes, placeholders, labels, and optional captions.
    - Styled the input field and label for better user experience.

---
+++ components/CustomInput.tsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  TextStyle,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+interface InputProps extends TextInputProps {
+  value: string;
+  extraStyle?: TextStyle;
+  onChangeText: (text: string) => void;
+  placeholder: "Your password" | "Your Email Address";
+  label: "Email" | "Password";
+  onBlur?: () => void;
+  caption?: string;
+  status?: string;
+  password?: boolean;
+}
+
+const EyeIcon = ({ isPassword }: { isPassword: boolean }) => {
+  const name = isPassword ? "eye-off-outline" : "eye-outline";
+  return <MaterialCommunityIcons name={name} size={24} color={"black"} />;
+};
+
+const CustomInput = ({
+  value,
+  extraStyle,
+  onChangeText,
+  placeholder,
+  label,
+  onBlur,
+  caption,
+  status,
+  password,
+  secureTextEntry,
+  ...props
+}: InputProps) => {
+  const [isPassword, setIsPassword] = useState<boolean>(true);
+  console.log({ caption, status });
+
+  return (
+    <>
+      <Text style={[styles.label]}>{label}</Text>
+      <View style={[styles.container]}>
+        <TextInput
+          style={[styles.inputStyle, extraStyle]}
+          value={value}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          secureTextEntry={password ? isPassword : false}
+          onBlur={onBlur}
+          {...props}
+        />
+        {password && (
+          <TouchableOpacity
+            onPress={() => setIsPassword(!isPassword)}
+            style={styles.iconPosition}
+          >
+            <EyeIcon isPassword={isPassword} />
+          </TouchableOpacity>
+        )}
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+    position: "relative",
+  },
+  label: {
+    color: "#574f4f",
+  },
+  inputStyle: {
+    backgroundColor: "#f6f6f6",
+    height: 40,
+    fontSize: 16,
+    borderColor: "#c5c5c5",
+    borderWidth: StyleSheet.hairlineWidth,
+    marginVertical: 4,
+    paddingHorizontal: 10,
+  },
+  iconPosition: { position: "absolute", right: 10, top: 12 },
+});
+
+export default CustomInput;
