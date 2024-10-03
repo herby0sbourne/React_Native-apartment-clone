f7e954f0dea327cf054ce6b2d2d3876901285f0c
Herby <herbyosbourne31@gmail.com>
2024-10-03 12:25:27 -0500

    create new component to show google and facebook auth button

---
+++ components/SocialAuthButton.tsx
@@ -0,0 +1,52 @@
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from "react-native";
+
+import { UtilStyles } from "@/constants/UtilStyles";
+
+import GoogleLogo from "@/assets/logos/google.svg";
+import FacebookLogo from "@/assets/logos/facebook-white.svg";
+
+export interface AuthButtonProps {
+  text: string;
+  extraStyle?: ViewStyle;
+  textStyle?: TextStyle;
+  onPress: () => void;
+  isLoading?: boolean;
+  type: "facebook" | "google";
+}
+
+const SocialAuthButton = ({
+  text,
+  extraStyle,
+  onPress,
+  textStyle,
+  isLoading,
+  type,
+}: AuthButtonProps) => {
+  const authLogo = {
+    facebook: <FacebookLogo width={24} height={24} />,
+    google: <GoogleLogo width={24} height={24} />,
+  };
+
+  return (
+    <TouchableOpacity style={[UtilStyles.authBtn, extraStyle]} onPress={onPress}>
+      {isLoading ? (
+        <ActivityIndicator size={25} color="white" />
+      ) : (
+        <>
+          {authLogo[type]}
+          <Text style={[UtilStyles.authText, textStyle]}>{text}</Text>
+        </>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default SocialAuthButton;
