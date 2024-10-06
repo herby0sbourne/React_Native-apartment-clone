e28596f7ac3006218ee6ffd7bf0daecac4133007
Herby <herbyosbourne31@gmail.com>
2024-10-06 09:26:45 -0500

    remove api config and others

--- constants/variable.ts
+++ constants/variable.ts
@@ -1,24 +1,4 @@
-import { Platform } from "react-native";
-
 const HEADER_HEIGHT = 250;
 const TRANSITION_THRESHOLD = 100;
 
-const IPAddress = Platform.OS === "android" ? "10.0.2.2" : "localhost";
-const SERVER_URL = `http://${IPAddress}:4000/api`;
-
-const location = "/location";
-const user = "/user";
-
-const locationEndpoint = SERVER_URL + location;
-const userEndpoint = SERVER_URL + user;
-
-const ENDPOINTS = {
-  search: `${locationEndpoint}/search`,
-  autocomplete: `${locationEndpoint}/autocomplete`,
-  login: `${userEndpoint}/login-user`,
-  google: `${userEndpoint}/google-login`,
-  register: `${userEndpoint}/create-user`,
-  facebook: `${userEndpoint}/facebook-login`,
-};
-
-export { HEADER_HEIGHT, TRANSITION_THRESHOLD, ENDPOINTS };
+export { HEADER_HEIGHT, TRANSITION_THRESHOLD };
