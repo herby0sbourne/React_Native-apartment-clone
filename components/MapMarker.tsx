0bdfb5317198770fff3e15fa64fa23ebcc672b41
Herby <herbyosbourne31@gmail.com>
2024-10-14 06:59:50 -0500

    added option for CallOut

--- components/MapMarker.tsx
+++ components/MapMarker.tsx
@@ -1,20 +1,23 @@
 import { View } from "react-native";
 import { Marker, MarkerPressEvent } from "react-native-maps";
 import { Ionicons } from "@expo/vector-icons";
+import { ReactNode } from "react";
 
 interface MapMarkerProps {
   lat: number;
   lng: number;
   onPress: (event: MarkerPressEvent) => void;
   color: string;
+  children?: ReactNode;
 }
 
-const MapMarker = ({ lat, lng, onPress, color }: MapMarkerProps) => {
+const MapMarker = ({ lat, lng, onPress, color, children }: MapMarkerProps) => {
   return (
     <Marker coordinate={{ latitude: lat, longitude: lng }} onPress={onPress}>
       <View>
         <Ionicons name="location-sharp" size={32} color={color} />
       </View>
+      {children}
     </Marker>
   );
 };
