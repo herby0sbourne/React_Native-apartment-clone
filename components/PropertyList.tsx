fe7a47f7792a3f2a48f33e4cf28ca5f711ed485a
Herby <herbyosbourne31@gmail.com>
2024-10-23 10:10:03 -0500

    added padding top to flatlist content

--- components/PropertyList.tsx
+++ components/PropertyList.tsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { router } from "expo-router";
+import { Platform, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import ReAnimated, { useAnimatedRef, useScrollViewOffset } from "react-native-reanimated";
+
+import SafeArea from "@/components/SafeArea";
 import NoProperty from "@/components/NoProperty";
 import PropertyCard from "@/components/PropertyCard";
-import ReAnimated, { useAnimatedRef, useScrollViewOffset } from "react-native-reanimated";
-import SafeArea from "@/components/SafeArea";
-import { router } from "expo-router";
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
-import { useCallback } from "react";
-import { RouteProp } from "@react-navigation/core";
 
 interface PropertyListProps {
   isMap: boolean;
@@ -14,6 +14,10 @@ interface PropertyListProps {
   setScroll: any;
   properties: Property[];
 }
+
+// const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+const SafeWrapper = Platform.OS === "ios" ? SafeAreaView : SafeArea;
 
 const PropertyList = ({ properties, isMap, route, setScroll }: PropertyListProps) => {
   const bottomHeight = useBottomTabBarHeight();
@@ -25,45 +29,41 @@ const PropertyList = ({ properties, isMap, route, setScroll }: PropertyListProps
     router.push(`/property/${propertyId}`);
   };
 
-  // const handleScroll = (scrollValue) => {
-  //   setScroll(scrollValue);
-  //   console.log(" you scrolled ", scrollValue.value);
-  // };
-
-  // const handleScroll = (event) => {
-  //   const offset = event.nativeEvent.contentOffset.y;
-  //   setScroll(offset); // Call setScroll with the new offset
-  // };
   const handleScroll = () => {
     setScroll(scrollOffset);
   };
 
   return (
-    <SafeArea>
+    <SafeWrapper>
       <ReAnimated.FlatList
         onScroll={handleScroll}
-        // onScroll={() => handleScroll(scrollOffset)}
         ref={flatListRef}
         data={properties}
         scrollEnabled={!!properties.length}
         ListEmptyComponent={<NoProperty isSearch={!!route.params} />}
         style={{
           backgroundColor: "white",
-          // zIndex: 5,
-          // display: isMap ? "none" : "flex",
+          zIndex: 8,
+          display: isMap ? "none" : "flex",
         }}
         renderItem={({ item }) => (
           <PropertyCard property={item} onPress={() => navigateToProperty(item.id)} />
         )}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={[
-          { gap: 10, paddingHorizontal: 10, paddingBottom: bottomHeight },
+          {
+            gap: 10,
+            paddingTop: 180,
+            paddingHorizontal: 10,
+            paddingBottom: bottomHeight,
+            backgroundColor: "white",
+          },
         ]}
         showsVerticalScrollIndicator={false}
         scrollEventThrottle={16}
         bounces={false}
       />
-    </SafeArea>
+    </SafeWrapper>
   );
 };
 
