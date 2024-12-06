c3728ecde2c97eab8d37ac0ab154df3e66ac4961
Herby <herbyosbourne31@gmail.com>
2024-12-06 17:34:39 -0500

    switch to react hook for learning purpose

--- app/messageScreen.tsx
+++ app/messageScreen.tsx
@@ -1,16 +1,18 @@
-import { properties } from "@/data/properties";
-import { router, useLocalSearchParams } from "expo-router";
-import { StyleSheet, Text, View, Image, Pressable } from "react-native";
-import DateTimePicker from "@react-native-community/datetimepicker";
 import { useState } from "react";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { abvState } from "@/utils/abbrevState";
-import { Formik } from "formik";
-import useAuth from "@/hooks/useAuth";
 import { object, string } from "yup";
-import CustomInput from "@/components/CustomInput";
-import { captionStatus } from "@/utils/captionStatus";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { router, useLocalSearchParams } from "expo-router";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import useAuth from "@/hooks/useAuth";
 import Button from "@/components/Button";
+import CustomInput from "@/components/CustomInput";
+
+import { abvState } from "@/utils/abbrevState";
+import { properties } from "@/data/properties";
 
 // import RNDateTimePicker from "@react-native-community/datetimepicker";
 
@@ -19,6 +21,14 @@ interface SearchParams extends Record<string, string | undefined> {
   tour?: boolean;
 }
 
+const schema = object().shape({
+  firstName: string().required("Your first name is required"),
+  lastName: string().required("Your last name is required"),
+  email: string().email().required("Your email is required"),
+  phoneNumber: string(),
+  message: string().required("Required"),
+});
+
 const MessageScreen = () => {
   const { user } = useAuth();
   const [pickedDate, setPickedDate] = useState<Date>(new Date());
@@ -26,10 +36,31 @@ const MessageScreen = () => {
   const { propertyId, tour } = useLocalSearchParams() as SearchParams;
   const property = properties.find((property) => +propertyId === property.id);
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, touched },
+    setValue,
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      firstName: user ? user.firstName : "",
+      lastName: user?.lastName ?? "",
+      phoneNumber: "",
+      email: user?.email ?? "",
+      message: tour ? "I would like to schedule a tour." : "",
+    },
+  });
+
   if (!property) {
     router.back();
     return null;
   }
+
+  const onSubmit = (data) => {
+    console.log("message log", data);
+    // router.back();
+  };
 
   return (
     <KeyboardAwareScrollView>
@@ -48,124 +79,118 @@ const MessageScreen = () => {
             </Text>
           </View>
         </View>
-        <Formik
-          initialValues={{
-            firstName: user ? user.firstName : "",
-            lastName: user?.lastName ?? "",
-            phoneNumber: "",
-            email: user?.email ?? "",
-            message: tour ? "I would like to schedule a tour." : "",
-          }}
-          validationSchema={object().shape({
-            firstName: string().required("Your first name is required"),
-            lastName: string().required("Your last name is required"),
-            email: string().email().required("Your email is required"),
-            phoneNumber: string(),
-            message: string().required("Required"),
-          })}
-          onSubmit={(values) => {
-            console.log("message log", values);
-            // router.back()
-          }}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-            setFieldTouched,
-            setFieldValue,
-          }) => {
-            return (
-              <>
-                <CustomInput
-                  label={"First Name*"}
-                  value={values.firstName}
-                  keyboardType={"default"}
-                  onChangeText={handleChange("firstName")}
-                  onBlur={() => setFieldTouched("firstName")}
-                  placeholder={"Your First Name"}
-                  autoCapitalize="none"
-                  caption={captionStatus(touched, errors, "firstName").caption}
-                  status={captionStatus(touched, errors, "firstName").status}
-                />
-                <CustomInput
-                  label={"Last Name*"}
-                  value={values.lastName}
-                  onChangeText={handleChange("lastName")}
-                  onBlur={() => setFieldTouched("lastName")}
-                  placeholder={"Your Last Name"}
-                  autoCapitalize="none"
-                  caption={captionStatus(touched, errors, "lastName").caption}
-                  status={captionStatus(touched, errors, "lastName").status}
-                />
-                <CustomInput
-                  value={values.phoneNumber}
-                  onChangeText={handleChange("phoneNumber")}
-                  placeholder="Your phone Number"
-                  label="Phone Number"
-                  keyboardType={"number-pad"}
-                />
-                <CustomInput
-                  label={"Email*"}
-                  value={values.email}
-                  keyboardType={"email-address"}
-                  onChangeText={handleChange("email")}
-                  onBlur={() => setFieldTouched("email")}
-                  placeholder={"Your email"}
-                  autoCapitalize="none"
-                  caption={captionStatus(touched, errors, "email").caption}
-                  status={captionStatus(touched, errors, "email").status}
-                />
+        <Controller
+          control={control}
+          name="firstName"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <CustomInput
+              label={"First Name"}
+              value={value}
+              required
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={"Your First Name"}
+              autoCapitalize="none"
+              caption={errors.firstName?.message}
+              status={errors.firstName ? "error" : "success"}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="lastName"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <CustomInput
+              label={"Last Name"}
+              value={value}
+              required
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={"Your Last Name"}
+              autoCapitalize="none"
+              caption={errors.lastName?.message}
+              status={errors.lastName ? "error" : "success"}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="phoneNumber"
+          render={({ field: { onChange, value } }) => (
+            <CustomInput
+              value={value}
+              onChangeText={onChange}
+              placeholder="Your phone Number"
+              label="Phone Number"
+              keyboardType={"number-pad"}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <CustomInput
+              label={"Email"}
+              required
+              value={value}
+              keyboardType={"email-address"}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={"Your email"}
+              autoCapitalize="none"
+              caption={errors.email?.message}
+              status={errors.email ? "error" : "success"}
+            />
+          )}
+        />
 
-                <View style={styles.label}>
-                  <Text>Move-In Date</Text>
-                  <Pressable
-                    style={styles.pickedDate}
-                    onPress={() => setShowCalender(true)}
-                  >
-                    <Text style={styles.dateText}>{pickedDate?.toDateString()}</Text>
-                  </Pressable>
-                </View>
-                {showCalender && (
-                  <DateTimePicker
-                    mode="date"
-                    value={pickedDate}
-                    onChange={(event, date) => {
-                      if (!date) return;
+        <View style={styles.label}>
+          <Text>Move-In Date</Text>
+          <Pressable style={styles.pickedDate} onPress={() => setShowCalender(true)}>
+            <Text style={styles.dateText}>{pickedDate?.toDateString()}</Text>
+          </Pressable>
+        </View>
+        {showCalender && (
+          <DateTimePicker
+            mode="date"
+            value={pickedDate}
+            onChange={(event, date) => {
+              if (!date) return;
 
-                      setPickedDate(date);
-                      setShowCalender(false);
-                      console.log(date);
-                    }}
-                  />
-                )}
-                <CustomInput
-                  label={"Message*"}
-                  value={values.message}
-                  keyboardType={"default"}
-                  onChangeText={handleChange("message")}
-                  onBlur={() => setFieldTouched("message")}
-                  placeholder={"Your message"}
-                  autoCapitalize="none"
-                  multiline={true}
-                  numberOfLines={4}
-                  extraStyle={styles.messageInput}
-                  caption={captionStatus(touched, errors, "message").caption}
-                  status={captionStatus(touched, errors, "message").status}
-                />
+              setPickedDate(date);
+              setShowCalender(false);
+              console.log(date);
+            }}
+          />
+        )}
+        <Controller
+          control={control}
+          name="message"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <CustomInput
+              label={"Message"}
+              value={value}
+              required
+              keyboardType={"default"}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              placeholder={"Your message"}
+              autoCapitalize="none"
+              multiline={true}
+              numberOfLines={4}
+              extraStyle={styles.messageInput}
+              caption={errors.message?.message}
+              status={errors.message ? "danger" : "basic"}
+            />
+          )}
+        />
 
-                <Button
-                  title={"Send Message"}
-                  ghostBtn={false}
-                  onPress={() => handleSubmit()}
-                />
-              </>
-            );
-          }}
-        </Formik>
+        <Button
+          title={"Send Message"}
+          ghostBtn={false}
+          onPress={handleSubmit(onSubmit)}
+        />
       </View>
 
       {/* </SafeArea> */}
